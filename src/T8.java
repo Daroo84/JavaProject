fe3b9fd95dab1ccd882e2d1170b3e68ce2e2df3f
Daroo84 <darekstaron@gmail.com>
2018-08-03 13:30:04 +0200

    T8

--- src/T8.java
+++ src/T8.java
@@ -3,10 +3,23 @@ import java.util.Scanner;
 public class T8 {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.ini);
-
+        Scanner scanner = new Scanner(System.in);
+        /*
         System.out.println("Enter a number: ");
 
-        int value = 
+        int value = scanner.nextInt();
+
+        while (value !=5) {
+            System.out.println("Enter a number: ");
+            value = scanner.nextInt();
+        }
+        */
+        do {
+            System.out.println("Enter a number: ");
+            value = scanner.nextInt();
+        }
+        while(value !=5);
+
+        System.out.println("Got 5!");
     }
 }
