31bc123c84a74749379067f7e4c9f951d36d2451
Daroo84 <darekstaron@gmail.com>
2018-09-11 11:12:43 +0200

    Added substract with JOptionPane

--- src/codeacademy/Main.java
+++ src/codeacademy/Main.java
@@ -1,12 +1,25 @@
 package codeacademy;
 
+import javax.swing.*;
+
 public class Main {
 
-    int sum(int a, int b){
-        return a+b;
+    int sum(int a, int b) {
+        return a + b;
     }
-    static int multiply(int a, int b){
-        return a*b;
+
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    static int add(int a, int b, int c){
+        int sum = a + b + c;
+        return sum;
+
+    }
+    static int substract(int a, int b){
+        int score1 = a - b;
+        return score1;
     }
 
     public static void main(String[] args) {
@@ -18,6 +31,12 @@ public class Main {
 
         int score = (multiply(10,9));
         System.out.println(score);
+        System.out.println(add(5,5,5));
+
+        int a = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter first number: "));
+        int b = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter second no.: "));
+        int score1 = substract(a,b);
+        JOptionPane.showMessageDialog(null, "Substract is: " +score1);
 
     }
 }
