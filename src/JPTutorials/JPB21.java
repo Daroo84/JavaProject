5046e468c8c2f61190b34710adbd2e9a81f90492
Daroo84 <darekstaron@gmail.com>
2018-09-20 16:52:36 +0200

    Java Tasks 4

---
+++ src/JPTutorials/JPB21.java
@@ -0,0 +1,17 @@
+package JPTutorials;
+
+import java.util.Scanner;
+
+public class JPB21 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int total = 0;
+        int grade;
+        int avarage;
+        int counter = 0;
+
+        while (counter < 10){
+            grade
+        }
+    }
+}
